e8364fe5a41103a450799e35cf599008fbcf55db
notNinja <54367234+godwin8030@users.noreply.github.com>
2021-06-26 17:25:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic Web Dev React Js Bootcamp. Thank you Shaurya for the bootcamp.</p>
+    </div>
+  );
+}
+
+export default Header;
